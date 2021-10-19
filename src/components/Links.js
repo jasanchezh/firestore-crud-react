f7f141833a38d67153cd4a24e5643f864c0a6408
Jose Sanchez <joseansanher@gmail.com>
2021-10-19 18:40:52 +0100

    some functions fixed

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LinkForm from "./LinkForm";
 
 import { db } from "../firebase";
-import { addDoc, collection, query, onSnapshot } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  // getDocs,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
 
 const Links = () => {
   const addOrEditLink = async (linkObject) => {
     try {
-      const allTask = [];
       const q = query(collection(db, "links"));
       await addDoc(q, linkObject);
-      onSnapshot(q, (querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          allTask.push(doc.data());
-        });
-      });
-      console.log("allTask");
-      console.log(allTask);
       console.log("New task created");
     } catch (e) {
       console.log(e);
     }
   };
+
+  const getLinks = () => {
+    try {
+      const allTasks = [];
+      const q = query(collection(db, "links"));
+      onSnapshot(q, (querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log("doc.data()");
+          console.log(doc.data());
+          allTasks.push(doc.data());
+        });
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getLinks();
+  }, []);
 
   return (
     <div>
